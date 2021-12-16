3abbcbbdf13397f7bdb44cf8cc64fbb887eb4239
Kais <kaisakrusenberg@gmail.com>
2021-12-16 14:54:00 +0200

    popup lehed ühendatud, salvestus pooleli

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <v-app class="grey lighten-4">
     <Navbar/>
     <PopupDialogue/>
-    <PopupDialogue2/>
-    <PopupDialogue3/>
-    <PopupDialogue4/>
-    <PopupDialogue5/>
-
    <v-main>
      <router-view></router-view>
    </v-main>
@@ -17,14 +12,10 @@
 <script>
 import Navbar from "./components/layout/Navbar";
 import PopupDialogue from "./pages/PopupDialogue1";
-import PopupDialogue2 from "./pages/PopupDialogue2";
-import PopupDialogue3 from "./pages/PopupDialogue3";
-import PopupDialogue4 from "./pages/PopupDialogue4";
-import PopupDialogue5 from "./pages/PopupDialogue5";
 
 export default {
   name: 'App',
-  components: {PopupDialogue, PopupDialogue2, PopupDialogue3, PopupDialogue4, PopupDialogue5, Navbar},
+  components: {PopupDialogue, Navbar},
 
   data () {
     return {
--- src/pages/PopupDialogue3.vue
+++ src/pages/PopupDialogue3.vue
@@ -1,108 +1,20 @@
 
   <template>
 
+    <div class="listContainer">
+      <PopupDialogue2Content v-if="thirdDialogCompleted" />
 
-    <v-row justify="center">
-      <v-dialog
-          v-model="dialog"
-          fullscreen
-          hide-overlay
-          transition="dialog-bottom-transition"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-              class="ma-auto"
-              color="teal darken-1 "
-              dark
-              v-bind="attrs"
-              v-on="on"
-          >
-            3.leht (pealkiri)
-          </v-btn>
-        </template>
-        <v-card>
-          <v-toolbar
-              dark
-              color="#1D2A3E"
-          >
-            <v-btn href="http://localhost:8081/" text
-                icon
-                dark
-                @click="console.log('clicked')"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-toolbar-title></v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-              <v-btn
-                  dark
-                  text
-                  @click="console.log('clicked')"></v-btn>
-            </v-toolbar-items>
-          </v-toolbar>
-          <div class="listContainer">
-            <v-list class="miskit3"
-                subheader
-            >
-              <v-list-item style="margin-top: 50px">
-                <v-text-field style="margin-top: 50px"
-                      v-model="value"
-                      :rules="[rules.required]"
-                      label="Pealkiri"
-                      counter
-                      maxlength="30"
-                />
-              </v-list-item>
-
-              <v-list-item style="margin-top: 10px">
-                <v-text-field
-                              v-model="value2"
-                              :rules="[rules.required]"
-                              label="Kuulutuse tekst"
-                              counter
-                              maxlength="250"
-                />
-              </v-list-item>
-
-              <v-combobox style="padding: 8px 0px"
-                          multiple
-                          v-model="select"
-                          label="Märksõnad"
-                          append-icon
-                          chips
-                          deletable-chips
-                          class="tag-input"
-                          :search-input.sync="search"
-                          @keyup.tab="updateTags"
-                          @paste="updateTags">
-              </v-combobox>
-            </v-list>
-
-            <img :scr="image"/>
-
-
-              <v-btn elevation="2" x-large class="blue-grey darken-4 white--text automargin">
-                <v-icon>mdi-chevron-left</v-icon>
-                <span>Tagasi</span>
-              </v-btn>
-              <v-btn  elevation="2" x-large class="teal darken-1 white--text automargin">Salvesta ja edasi
-                <v-icon>mdi-chevron-right</v-icon>
-              </v-btn>
-            </div>
-
-        </v-card>
-      </v-dialog>
-    </v-row>
+    </div>
   </template>
 
 
 <script>
+import PopupDialogue4Content from "./PopupDialogue4";
 export default {
   name: "PopupDialogue3",
   data () {
     return {
-      loading: false,
+      thirdDialogCompleted: false,
       dialog: '',
       value: '',
       value2: '',
@@ -122,10 +34,7 @@ export default {
     }
   },
   methods: {
-    getTags () {
-      this.$http.get('api/listing/tags')
-          .then (result => this.tags = result.data)
-    },
+
   },
   // beforeMount() {
   //   const categories = this.getCategories();
@@ -140,13 +49,7 @@ export default {
 
 <style scoped>
 
-.miskit3 {
-  padding: 0 50px 100px 50px;
-}
 
-.automargin {
-  margin: auto;
-}
 
 
 .listContainer {
